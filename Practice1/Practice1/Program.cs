737c96cc83fec05a4ef3438abc2b301a31cbc723
ykvit <yumkvitka+github@gmail.com>
2024-04-25 23:59:36 +0300

    testend

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -8,10 +8,6 @@ using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 
-
-// test
-// test 2
-// test 3
 namespace Practice1
 {
     internal static class Program
